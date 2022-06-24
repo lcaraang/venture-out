3ef874416a66bb52a7d95b2b4b81d78a27765482
Louella Caraang <lcaraang@hawaii.edu>
2022-06-24 10:05:57 -1000

    add modal

--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,6 @@ from django.utils import timezone
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='images/')
-    caption = models.CharField(max_length=200)
+    caption = models.TextField(max_length=200)
     date = models.DateTimeField(default=timezone.now)
 
